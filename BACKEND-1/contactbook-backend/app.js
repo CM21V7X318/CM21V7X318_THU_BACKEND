6976e8381ea8c4cfda659703ec8f8bc5f7f72a10
CM21V7X318 <155377675+CM21V7X318@users.noreply.github.com>
2024-01-09 18:40:26 +0700

    Cai dat xu ly loi

--- BACKEND-1/contactbook-backend/app.js
+++ BACKEND-1/contactbook-backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +15,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
